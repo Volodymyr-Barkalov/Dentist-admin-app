edf6d4fc6e32fa9289f1c69d8e8613b8b7feffce
Volodymyr Barkalov <vobarkal@gmail.com>
2019-08-14 23:02:41 +0300

    First page input form view

--- src/components/InputForm.js
+++ src/components/InputForm.js
@@ -7,15 +7,17 @@ class InputForm extends Component {
         var date = new Date();
         return (
             <form>
-                <div className="form-group">
-                    <label htmlFor="patientNo">Медична картка стоматологічного хворого №</label>
-                    <input type="text" className="form-control" id="patientNo"
-                           placeholder="Введіть номер медичної картки стоматологічного хворого"/>
-                </div>
-                <div className="form-group">
-                    <label htmlFor="year">Рік</label>
-                    <input type="text" className="form-control" id="year"
-                           placeholder={date.getFullYear()}/>
+                <div className="form-row">
+                    <div className="col">
+                        <label htmlFor="patientNo">Медична картка стоматологічного хворого №</label>
+                        <input type="text" className="form-control" id="patientNo"
+                               placeholder="Введіть номер медичної картки стоматологічного хворого"/>
+                    </div>
+                    <div className="col">
+                        <label htmlFor="year">Рік</label>
+                        <input type="text" className="form-control" id="year"
+                               placeholder={date.getFullYear()}/>
+                    </div>
                 </div>
                 <div className="form-group">
                     <label htmlFor="lastName">Прізвище</label>
@@ -31,6 +33,53 @@ class InputForm extends Component {
                     <label htmlFor="fathersName">По-батькові</label>
                     <input type="text" className="form-control" id="fathersName"
                            placeholder="Іванович"/>
+                </div>
+                <div className="form-row form-group">
+
+                        <div className="form-group col-md-4">
+                            <label htmlFor="gender">Стать</label>
+                            <select id="gender" className="form-control">
+                                <option selected>Оберіть стать...</option>
+                                <option>Чоловіча</option>
+                                <option>Жіноча</option>
+                            </select>
+                        </div>
+
+                        <div className="col">
+                            <label htmlFor="birthday">Рік народження</label>
+                            <input type="text" className="form-control" id="birthday"
+                                   placeholder="01.01.1990"/>
+                        </div>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="adress">Місце проживання хворого</label>
+                    <input type="text" className="form-control" id="adress"
+                           placeholder="Адреса"/>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="phone">Телефон</label>
+                    <input type="text" className="form-control" id="phone"
+                           placeholder="+380991111111"/>
+                </div>
+
+                <div className="form-group">
+                    <label htmlFor="Diagnosis">Діагноз</label>
+                    <textarea className="form-control" id="Diagnosis" rows="3"></textarea>
+                </div>
+
+                <div className="form-group">
+                    <label htmlFor="complaints">Скарги</label>
+                    <textarea className="form-control" id="complaints" rows="3"></textarea>
+                </div>
+
+                <div className="form-group">
+                    <label htmlFor="pastIllnes">Перенесені та супутні захворювання</label>
+                    <textarea className="form-control" id="pastIllnes" rows="3"></textarea>
+                </div>
+
+                <div className="form-group">
+                    <label htmlFor="disease">Розвиток теперішнього захворювання</label>
+                    <textarea className="form-control" id="disease" rows="3"></textarea>
                 </div>
 
                 <button type="submit" className="btn btn-primary">Submit</button>
