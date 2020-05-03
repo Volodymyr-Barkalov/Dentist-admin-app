dde7c8d3dcfcdc94f0dc70a4b38467e581d1827a
Volodymyr Barkalov <vobarkal@gmail.com>
2020-05-03 23:23:33 +0300

    remove localhost

--- src/main/react/src/actions/formActions.js
+++ src/main/react/src/actions/formActions.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const addForm = (form43, history) => async dispatch => {
-    await axios.post("http://localhost:8080/form/add", form43);
+    await axios.post("/form/add", form43);
     history.push("/");
 }
