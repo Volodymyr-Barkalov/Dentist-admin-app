aff9d52ad71e6ce9f3ba84074818105c50b07056
Volodymyr Barkalov <vobarkal@gmail.com>
2020-05-10 23:58:22 +0300

    refactor dir tree

--- dentistappjava/src/main/react/src/reducers/index.js
+++
@@ -1,7 +0,0 @@
-import {combineReducers} from "redux";
-import errorReducer from "./errorReducer";
-
-export default combineReducers({
-    //
-    errors: errorReducer
-});
--- dentistappjava/src/main/react/src/util/getCurrentHostName.js
+++
@@ -1,8 +0,0 @@
-//returns a string with current host name, e.g: 'http://localhost:3000/' or 'https://dentist-admin-app.herokuapp.com'
-function getCurrentHostName() {
-    const currentPageURL = window.location.href;
-    const urlArray = currentPageURL.split("/");
-    return urlArray[0] + "//" + urlArray[2];
-}
-
-export default getCurrentHostName;
